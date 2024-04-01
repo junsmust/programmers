38fa24db75a766d89b435f7d63f6e11c923b5207
임주영 <isp3087@gmail.com>
2024-04-02 00:27:37 +0900

    최솟값 만들기
    startIndex 를 +1 만큼 증감하며 전체 탐색을 했었는데 예제 코드는 맞았지만, 적합성이나 효율성은 다 틀렸음
    
    다른 예제를 보니 큰 수에 작은 수를 곱하니 최솟값을 구할 수 있었다.
    
    ..어렵당

--- Practice/main.swift
+++ Practice/main.swift
@@ -7,4 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+
+//func solution(_ A:[Int], _ B:[Int]) -> Int
+//{
+//   var ans = 0
+//
+//   var sums: [Int] = []
+//   var startIndex: Int = 0
+//   
+//   while startIndex < A.count {
+//       var sum: Int = 0
+//       for i in 0..<A.count {
+//           let a = A[i]
+//           let b = B[(i+startIndex)%B.count]
+//           sum += (a+b)
+//       }
+//       sums.append(sum)
+//       startIndex += 1
+//   }
+//   print("sums : \(sums)")
+//
+//
+//   return ans
+//}
+
+//soluti
+
+let t = solution([1, 4, 2],[5, 4, 4])
+print("t : \(t)")
+
